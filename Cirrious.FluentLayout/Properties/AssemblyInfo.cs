2323846805a98e0660feaefcdfdd63907e990812
Greg Shackles <gshackles@gmail.com>
2016-03-05 16:07:22 -0500

    bump version number to 2.5

--- Cirrious.FluentLayout/Properties/AssemblyInfo.cs
+++ Cirrious.FluentLayout/Properties/AssemblyInfo.cs
@@ -13,11 +13,11 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 
 [assembly: AssemblyTitle("Cirrious.FluentLayout")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Easy API for using AutoLayout in iOS")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Cirrious.FluentLayout")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -42,5 +42,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.5.0.0")]
+[assembly: AssemblyFileVersion("2.5.0.0")]
