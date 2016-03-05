4ad07bd789d95dbcf78b45d77ed117af007fb986
Greg Shackles <gshackles@gmail.com>
2016-03-05 17:32:42 -0500

    FluentLayoutExtensions: clean up with C# 6

--- Cirrious.FluentLayout/FluentLayoutExtensions.cs
+++ Cirrious.FluentLayout/FluentLayoutExtensions.cs
@@ -21,65 +21,29 @@ namespace Cirrious.FluentLayouts.Touch
             }
         }
 
-        public static UIViewAndLayoutAttribute Left(this UIView view)
-        {
-            return view.WithLayoutAttribute(NSLayoutAttribute.Left);
-        }
+        public static UIViewAndLayoutAttribute Left(this UIView view) => view.WithLayoutAttribute(NSLayoutAttribute.Left);
 
-        public static UIViewAndLayoutAttribute Right(this UIView view)
-        {
-            return view.WithLayoutAttribute(NSLayoutAttribute.Right);
-        }
+        public static UIViewAndLayoutAttribute Right(this UIView view) => view.WithLayoutAttribute(NSLayoutAttribute.Right);
 
-        public static UIViewAndLayoutAttribute Top(this UIView view)
-        {
-            return view.WithLayoutAttribute(NSLayoutAttribute.Top);
-        }
+		public static UIViewAndLayoutAttribute Top(this UIView view) => view.WithLayoutAttribute(NSLayoutAttribute.Top);
 
-        public static UIViewAndLayoutAttribute Bottom(this UIView view)
-        {
-            return view.WithLayoutAttribute(NSLayoutAttribute.Bottom);
-        }
+        public static UIViewAndLayoutAttribute Bottom(this UIView view) => view.WithLayoutAttribute(NSLayoutAttribute.Bottom);
 
-        public static UIViewAndLayoutAttribute Baseline(this UIView view)
-        {
-            return view.WithLayoutAttribute(NSLayoutAttribute.Baseline);
-        }
+        public static UIViewAndLayoutAttribute Baseline(this UIView view) => view.WithLayoutAttribute(NSLayoutAttribute.Baseline);
 
-        public static UIViewAndLayoutAttribute Trailing(this UIView view)
-        {
-            return view.WithLayoutAttribute(NSLayoutAttribute.Trailing);
-        }
+        public static UIViewAndLayoutAttribute Trailing(this UIView view) => view.WithLayoutAttribute(NSLayoutAttribute.Trailing);
 
-        public static UIViewAndLayoutAttribute Leading(this UIView view)
-        {
-            return view.WithLayoutAttribute(NSLayoutAttribute.Leading);
-        }
+        public static UIViewAndLayoutAttribute Leading(this UIView view) => view.WithLayoutAttribute(NSLayoutAttribute.Leading);
 
-        public static UIViewAndLayoutAttribute CenterX(this UIView view)
-        {
-            return view.WithLayoutAttribute(NSLayoutAttribute.CenterX);
-        }
+        public static UIViewAndLayoutAttribute CenterX(this UIView view) => view.WithLayoutAttribute(NSLayoutAttribute.CenterX);
 
-        public static UIViewAndLayoutAttribute CenterY(this UIView view)
-        {
-            return view.WithLayoutAttribute(NSLayoutAttribute.CenterY);
-        }
+        public static UIViewAndLayoutAttribute CenterY(this UIView view) => view.WithLayoutAttribute(NSLayoutAttribute.CenterY);
 
-        public static UIViewAndLayoutAttribute Height(this UIView view)
-        {
-            return view.WithLayoutAttribute(NSLayoutAttribute.Height);
-        }
+        public static UIViewAndLayoutAttribute Height(this UIView view) => view.WithLayoutAttribute(NSLayoutAttribute.Height);
 
-        public static UIViewAndLayoutAttribute Width(this UIView view)
-        {
-            return view.WithLayoutAttribute(NSLayoutAttribute.Width);
-        }
+        public static UIViewAndLayoutAttribute Width(this UIView view) => view.WithLayoutAttribute(NSLayoutAttribute.Width);
 
-        public static UIViewAndLayoutAttribute WithLayoutAttribute(this UIView view, NSLayoutAttribute attribute)
-        {
-            return new UIViewAndLayoutAttribute(view, attribute);
-        }
+        public static UIViewAndLayoutAttribute WithLayoutAttribute(this UIView view, NSLayoutAttribute attribute) => new UIViewAndLayoutAttribute(view, attribute);
 
 		public static void AddConstraints(this UIView view, params FluentLayout[] fluentLayouts) => view.AddConstraints(fluentLayouts.AsEnumerable());
 
