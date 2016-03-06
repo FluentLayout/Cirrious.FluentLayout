14cf15ddd94e9eeffbc9e595f5be6a464d16da26
Greg Shackles <gshackles@gmail.com>
2016-03-06 11:34:12 -0500

    Add NuGet package/publish to build script

--- build.fsx
+++ build.fsx
@@ -1,6 +1,8 @@
 #r @"packages/FAKE.4.21.0/tools/NuGet.Core.dll"
 #r @"packages/FAKE.4.21.0/tools/FakeLib.dll"
 
+open System.IO
+open System.Linq
 open Fake
 
 let exec command args =
@@ -9,15 +11,30 @@ let exec command args =
   if result <> 0 then failwithf "%s exited with error %d" command result
 
 Target "restore" (fun () ->
-  exec "tools/NuGet.exe" "restore QuickLayout.sln"
+  exec "tools/nuget.exe" "restore QuickLayout.sln"
 )
 
 Target "build" (fun () ->
-  MSBuild null "Build" [ "Configuration", "Debug" ] [ "QuickLayout.sln" ] 
+  MSBuild null "Build" [ "Configuration", "Release" ] [ "QuickLayout.sln" ] 
   |> ignore
 )
 
-"restore"
-  ==> "build"
+Target "nuget-package" (fun () ->
+  if Directory.Exists("dist") then Directory.Delete("dist", true)
+  Directory.CreateDirectory("dist") |> ignore
+
+  exec "tools/nuget.exe" "pack nuspec/Cirrious.FluentLayout.nuspec -NoDefaultExcludes -OutputDirectory dist"
+)
+
+Target "nuget-push" (fun () ->
+  let nupkgPath = Directory.EnumerateFiles("dist", "*.nupkg").First()
+
+  exec "tools/nuget.exe" ("push " + nupkgPath)
+)
+
+"restore" 
+  ==> "build" 
+  ==> "nuget-package"
+  ==> "nuget-push"
   
 RunTarget()
