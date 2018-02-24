c8a6f860942c5841e7888e2a9397dc825d8aeee1
Roger Weiss <roger@cowboyaliens.com>
2018-02-24 15:21:13 +0200

    Added Setup.cs file for MvvMCross boostrap Added References to Cirrious.FluentLayouts.tvOS project

---
+++ QuickLayout.TvApp/Setup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MvvmCross.Binding.Bindings.Target.Construction;
+using MvvmCross.Core.ViewModels;
+using MvvmCross.Platform.Platform;
+using MvvmCross.tvOS.Platform;
+using UIKit;
+
+namespace QuickLayout.TvApp
+{
+    public class Setup : MvxTvosSetup
+    {
+        public Setup(MvxApplicationDelegate applicationDelegate, UIWindow window)
+            : base(applicationDelegate, window)
+        {
+        }
+
+        protected override IMvxApplication CreateApp() => new Core.App();
+
+        protected override IMvxTrace CreateDebugTrace() => new DebugTrace();
+    }
+}
